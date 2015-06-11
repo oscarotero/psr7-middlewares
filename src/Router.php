371e56a2834423a47b1840779e564e17c60ecf1c
oscarotero <oom@oscarotero.com>
2015-06-11 23:06:17 +0200

    first commit

---
+++ src/Router.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Middlewares;
+
+use Aura\Router\RouterContainer;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class Router
+{
+	protected $router;
+
+	public function __construct()
+	{
+		$this->router = new RouterContainer();
+	}
+
+	public function getRouter()
+	{
+		return $this->router;
+	}
+
+	public function attach($namePrefix, $pathPrefix, $callback)
+	{
+		$this->router->getMap()->attach($namePrefix, $pathPrefix, $callback);
+	}
+
+	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+	{
+		$matcher = $this->router->getMatcher();
+        $route = $matcher->match($request);
+
+        if ($route) {
+            $response = call_user_func($route->handler, $request, $response);
+        }
+
+        return $next($request, $response);
+	}
+}
