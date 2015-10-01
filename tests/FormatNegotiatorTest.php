d0fd23ddfad453a2c250e0010ce35b2ec64afe5d
oscarotero <oom@oscarotero.com>
2015-10-01 12:12:42 +0200

    test

--- tests/FormatNegotiatorTest.php
+++ tests/FormatNegotiatorTest.php
@@ -9,7 +9,7 @@ class FormatNegotiatorTest extends Base
         return [
             [
                 '/',
-                'application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+                'application/xml;charset=UTF-8,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
                 'html',
             ],[
                 '/test.json',
