9ab631449ab3e1866371a5f2c9e87c25e56619b8
oscarotero <oom@oscarotero.com>
2015-10-03 12:11:30 +0200

    tests for read/write response middlewares

---
+++ tests/SaveReadResponseTest.php
@@ -0,0 +1,56 @@
+<?php
+use Psr7Middlewares\Middleware;
+
+class SaveReadResponseTest extends Base
+{
+    public function saveProvider()
+    {
+        return [
+            [
+                '/hello-world',
+                '/hello-world/index.html',
+                'Hello world',
+            ],[
+                '/post',
+                '/post/index.html',
+                'This is a post',
+            ],[
+                '/index.json',
+                '/index.json',
+                '{"hello": "world"}'
+            ],[
+                '/',
+                '/index.html',
+                'Index'
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider saveProvider
+     */
+    public function testSaveReadResponse($url, $file, $content)
+    {
+        $storage = __DIR__.'/tmp';
+
+        $this->dispatch(
+            [Middleware::saveResponse($storage)],
+            $this->request($url),
+            $this->response()->withBody($this->stream($content))
+        );
+
+        $this->assertTrue(is_file($storage.$file));
+        $this->assertEquals($content, file_get_contents($storage.$file));
+
+        $response = $this->execute(
+            [Middleware::readResponse($storage)],
+            $url
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($content, (string) $response->getBody());
+
+        unlink($storage.$file);
+        rmdir(dirname($storage.$file));
+    }
+}
