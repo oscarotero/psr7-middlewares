ceb5f010bf42acfaa8dcd30ee9e0a6d29a991bd5
oscarotero <oom@oscarotero.com>
2015-10-29 19:54:41 +0100

    added test for middleware::test

---
+++ tests/WhenTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Psr7Middlewares\Middleware;
+
+class WhenTest extends Base
+{
+    public function whenDataProvider()
+    {
+        return [
+            [false, 'Foo'],
+            [true, 'Bar,Foo'],
+            [function () {return false;}, 'Foo'],
+            [function () {return true;}, 'Bar,Foo'],
+        ];
+    }
+
+    /**
+     * @dataProvider whenDataProvider
+     */
+    public function testWhenFalse($condition, $result)
+    {
+        $response = $this->execute(
+            [
+                Middleware::When($condition, function ($request, $response, $next) {
+                    return $next($request, $response->withHeader('X-Foo', 'Bar'));
+                }),
+                function ($request, $response, $next) {
+                    return $next($request, $response->withAddedHeader('X-Foo', 'Foo'));
+                }
+            ]
+        );
+
+        $this->assertSame($result, $response->getHeaderLine('X-Foo'));
+    }
+}
