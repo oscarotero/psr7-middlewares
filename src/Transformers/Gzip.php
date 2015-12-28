d89578fa968c3afaefdaadd9bf5b21b7159cd392
oscarotero <oom@oscarotero.com>
2015-12-28 20:44:52 +0100

    added deflate encoding

--- src/Transformers/Gzip.php
+++ src/Transformers/Gzip.php
@@ -10,6 +10,7 @@ use Psr7Middlewares\Middleware;
  */
 class Gzip extends Resolver
 {
+    
     /**
      * Gzip minifier using gzencode()
      * 
@@ -24,6 +25,23 @@ class Gzip extends Resolver
 
         return $response
             ->withHeader('Content-Encoding', 'gzip')
+            ->withBody($stream);
+    }
+
+    /**
+     * Gzip minifier using gzdeflate()
+     * 
+     * @param ResponseInterface $response
+     * 
+     * @return ResponseInterface
+     */
+    public function deflate(ResponseInterface $response)
+    {
+        $stream = Middleware::createStream();
+        $stream->write(gzdeflate((string) $response->getBody()));
+
+        return $response
+            ->withHeader('Content-Encoding', 'deflate')
             ->withBody($stream);
     }
 }
