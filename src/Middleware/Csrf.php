2bae3137bce59e8cec93af5709f6f9e2d85a8dcd
oscarotero <oom@oscarotero.com>
2016-01-11 23:11:55 +0100

    removed paragonie/anti-csrf as a dependency

--- src/Middleware/Csrf.php
+++ src/Middleware/Csrf.php
@@ -8,14 +8,19 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use RuntimeException;
 use Exception;
-use ParagonIE\AntiCSRF\AntiCSRF;
 
 /**
  * Middleware for CSRF protection
+ * Code inspired from https://github.com/paragonie/anti-csrf
  */
 class Csrf
 {
     use Utils\FormTrait;
+
+    protected $maxTokens = 100;
+    protected $sessionIndex = 'CSRF';
+    protected $formIndex = '_CSRF_INDEX';
+    protected $formToken = '_CSRF_TOKEN';
 
     /**
      * Execute the middleware.
@@ -29,59 +34,137 @@ class Csrf
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         if (!Middleware::hasAttribute($request, FormatNegotiator::KEY)) {
-            throw new RuntimeException('FormTimestamp middleware needs FormatNegotiator executed before');
+            throw new RuntimeException('Csrf middleware needs FormatNegotiator executed before');
+        }
+
+        if (!Middleware::hasAttribute($request, ClientIp::KEY)) {
+            throw new RuntimeException('Csrf middleware needs ClientIp executed before');
+        }
+
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            throw new RuntimeException('Csrf middleware needs an active php session');
         }
 
         if (FormatNegotiator::getFormat($request) !== 'html') {
             return $next($request, $response);
         }
 
-        $csrf = $this->getCSRF($request);
-
-        if (Utils\Helpers::isPost($request) && (session_status() !== PHP_SESSION_ACTIVE || !$csrf->validateRequest())) {
+        if (Utils\Helpers::isPost($request) && !$this->validateRequest($request)) {
             return $response->withStatus(403);
         }
 
         $response = $next($request, $response);
 
-        $defaultAction = self::getDefaultAction($request);
-
-        return $this->insertIntoPostForms($response, function ($match) use ($csrf, $defaultAction) {
+        return $this->insertIntoPostForms($response, function ($match) use ($request) {
             preg_match('/action=["\']?([^"\'\s]+)["\']?/i', $match[0], $matches);
 
-            $action = empty($matches[1]) ? $defaultAction : $matches[1];
+            $action = empty($matches[1]) ? $request->getUri()->getPath() : $matches[1];
+            list($index, $token) = $this->getTokenArray($request, $action);
 
-            return $match[0].$csrf->insertToken($action, false);
+            return $match[0]
+                .'<input type="text" name="'.$this->formIndex.'" value="'.htmlentities($index, ENT_QUOTES, 'UTF-8').'">'
+                .'<input type="text" name="'.$this->formToken.'" value="'.htmlentities($token, ENT_QUOTES, 'UTF-8').'">';
         });
 
         return $response;
     }
 
     /**
-     * Creates an instance of AntiCSRF
-     * 
-     * @param ServerRequestInterface $request
-     * 
-     * @return AntiCSRF
+     * Retrieve a token array
+     *
+     * @return array
      */
-    private function getCSRF(ServerRequestInterface $request)
+    public function getTokenArray($request, $lockTo)
     {
-        $post = $request->getParsedBody();
-        $server = $request->getServerParams();
-
-        if (session_status() === PHP_SESSION_ACTIVE) {
-            $session =& $_SESSION;
-        } else {
-            $session = [];
+        if (!isset($_SESSION[$this->sessionIndex])) {
+            $_SESSION[$this->sessionIndex] = [];
         }
 
-        return new AntiCSRF($post, $session, $server);
+        $index = self::encode(random_bytes(18));
+        $token = self::encode(random_bytes(32));
+
+        $_SESSION[$this->sessionIndex][$index] = [
+            'created' => intval(date('YmdHis')),
+            'uri' => $request->getUri()->getPath(),
+            'token' => $token,
+            'lockTo' => $lockTo
+        ];
+
+        $this->recycleTokens();
+
+        $token = self::encode(hash_hmac('sha256', ClientIp::getIp($request), base64_decode($token), true));
+
+        return [$index, $token];
     }
 
-    private static function getDefaultAction(ServerRequestInterface $request)
+    /**
+     * Validate a request based on $this->session and $this->post data
+     *
+     * @return boolean
+     */
+    public function validateRequest($request)
     {
-        $server = $request->getServerParams();
+        if (!isset($_SESSION[$this->sessionIndex])) {
+            $_SESSION[$this->sessionIndex] = [];
+            return false;
+        }
 
-        return isset($server['REQUEST_URI']) ? $server['REQUEST_URI'] : $server['SCRIPT_NAME'];
+
+        $data = $request->getParsedBody();
+
+        if (!isset($data[$this->formIndex]) || !isset($data[$this->formToken])) {
+            return false;
+        }
+
+        $index = $data[$this->formIndex];
+        $token = $data[$this->formToken];
+
+        if (!isset($_SESSION[$this->sessionIndex][$index])) {
+            return false;
+        }
+
+        $stored = $_SESSION[$this->sessionIndex][$index];
+        unset($_SESSION[$this->sessionIndex][$index]);
+
+        $lockTo = $request->getUri()->getPath();
+
+        if (!hash_equals($lockTo, $stored['lockTo'])) {
+            return false;
+        }
+
+        $expected = self::encode(hash_hmac('sha256', ClientIp::getIp($request), base64_decode($stored['token']), true));
+
+        return hash_equals($token, $expected);
+    }
+
+    /**
+     * Enforce an upper limit on the number of tokens stored in session state
+     * by removing the oldest tokens first.
+     */
+    protected function recycleTokens()
+    {
+        if (!$this->maxTokens || count($_SESSION[$this->sessionIndex]) <= $this->maxTokens) {
+            return;
+        }
+
+        uasort($_SESSION[$this->sessionIndex], function($a, $b) {
+            return $a['created'] - $b['created'];
+        });
+
+        while (count($_SESSION[$this->sessionIndex]) > $this->maxTokens) {
+            array_shift($_SESSION[$this->sessionIndex]);
+        }
+    }
+
+    /**
+     * Encode string with base64, but strip padding.
+     * PHP base64_decode does not croak on that.
+     *
+     * @param string $s
+     * @return string
+     */
+    protected static function encode($s)
+    {
+        return rtrim(base64_encode($s), '=');
     }
 }
