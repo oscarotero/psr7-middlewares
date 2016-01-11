a84ef610779e1815bab4a60b7ec55b11a7816364
oscarotero <oom@oscarotero.com>
2016-01-11 21:20:40 +0100

    removed phpseclib dependency

--- src/Utils/CryptTrait.php
+++ src/Utils/CryptTrait.php
@@ -2,7 +2,6 @@
 
 namespace Psr7Middlewares\Utils;
 
-use phpseclib\Crypt\AES;
 use RuntimeException;
 
 /**
@@ -10,10 +9,11 @@ use RuntimeException;
  */
 trait CryptTrait
 {
-    private $cipher;
+    private $key;
+    private $authentication;
 
     /**
-     * Set the key.
+     * Set the keys to encrypt and authenticate.
      * 
      * @param string $key
      *
@@ -21,8 +21,8 @@ trait CryptTrait
      */
     public function key($key)
     {
-        $this->cipher = new AES();
-        $this->cipher->setKey($key);
+        $this->key = self::HKDF($key, 'KeyForEncryption');
+        $this->authentication = self::HKDF($key, 'KeyForAuthentication');
 
         return $this;
     }
@@ -36,11 +36,15 @@ trait CryptTrait
      */
     private function encrypt($value)
     {
-        if (empty($this->cipher)) {
-            throw new RuntimeException('No encrypt key provided');
+        if (empty($this->key) || empty($this->authentication)) {
+            throw new RuntimeException('No crypt keys provided');
         }
 
-        return bin2hex($this->cipher->encrypt($value));
+        $iv = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
+        $cipher = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->key, json_encode($value), 'ctr', $iv);
+        $hmac = hash_hmac('sha256', $iv.$cipher, $this->authentication, true);
+
+        return base64_encode($hmac.$iv.$cipher);
     }
 
     /**
@@ -52,10 +56,44 @@ trait CryptTrait
      */
     private function decrypt($value)
     {
-        if (empty($this->cipher)) {
-            throw new RuntimeException('No decrypt key provided');
+        if (empty($this->key) || empty($this->authentication)) {
+            throw new RuntimeException('No crypt keys provided');
         }
 
-        return $this->cipher->decrypt(hex2bin($value));
+        $decoded = base64_decode($value);
+        $hmac = mb_substr($decoded, 0, 32, '8bit');
+        $iv = mb_substr($decoded, 32, 16, '8bit');
+        $cipher = mb_substr($decoded, 48, null, '8bit');
+        $calculated = hash_hmac('sha256', $iv.$cipher, $this->authentication, true);
+    
+        if (hash_equals($hmac, $calculated)) {
+            $value = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->key, $cipher, 'ctr', $iv), "\0");
+            return json_decode($value, true);
+        }
+    }
+
+    /**
+     * Get derived key
+     * http://tools.ietf.org/html/rfc5869
+     * 
+     * @param string $ikm Initial Keying Material
+     * @param string $info What sort of key are we deriving?
+     * 
+     * @return string
+     */
+    private static function HKDF($ikm, $info = '')
+    {
+        $salt = str_repeat("\x00", 32);
+        $prk = hash_hmac('sha256', $ikm, $salt, true);
+
+        $t = '';
+        $last_block = '';
+
+        for ($block_index = 1; mb_strlen($t, '8bit') < 16; ++$block_index) {
+            $last_block = hash_hmac('sha256', $last_block.$info.chr($block_index), $prk, true);
+            $t .= $last_block;
+        }
+
+        return mb_substr($t, 0, 16, '8bit');
     }
 }
