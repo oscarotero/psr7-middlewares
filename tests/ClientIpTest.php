d27a2894b45620a057fdf705e38e60f8ecf8ceff
Wolfy-J <wolfy.jd@gmail.com>
2017-01-29 01:23:54 +0300

    serverOptions for ClientIp middleware

--- tests/ClientIpTest.php
+++ tests/ClientIpTest.php
@@ -10,18 +10,91 @@ class ClientIpTest extends Base
         return [
             [
                 [
-                    'Client-Ip' => 'unknow,123.456.789.10,123.234.123.10',
+                    'Client-Ip'   => 'unknow,123.456.789.10,123.234.123.10',
                     'X-Forwarded' => '123.234.123.10',
                 ],
                 ['123.234.123.10'],
                 '123.234.123.10',
-            ], [
+            ],
+            [
                 [
-                    'Client-Ip' => 'unknow,123.456.789.10,123.234.123.10',
+                    'Client-Ip'   => 'unknow,123.456.789.10,123.234.123.10',
                     'X-Forwarded' => '123.234.123.11',
                 ],
                 ['123.234.123.10', '123.234.123.11'],
                 '123.234.123.10',
+            ],
+        ];
+    }
+
+    public function srvProvider()
+    {
+        return [
+            [
+                [
+                    'REMOTE_ADDR' => '123.234.123.10'
+                ],
+                ['123.234.123.10'],
+                '123.234.123.10',
+            ],
+            [
+                [
+                    'REMOTE_ADDR'          => '123.234.123.10',
+                    'HTTP_X_FORWARDED_FOR' => '222.234.123.10'
+                ],
+                ['123.234.123.10', '222.234.123.10'],
+                '123.234.123.10',
+            ],
+            [
+                [
+                    'HTTP_X_FORWARDED_FOR' => '222.234.123.10',
+                    'REMOTE_ADDR'          => '123.234.123.10'
+                ],
+                ['123.234.123.10', '222.234.123.10'],
+                '123.234.123.10',
+            ],
+            [
+                [
+                    'HTTP_X_FORWARDED_FOR' => '222.234.123.10'
+                ],
+                ['222.234.123.10'],
+                '222.234.123.10',
+            ],
+        ];
+    }
+
+    public function srvReversedProvider()
+    {
+        return [
+            [
+                [
+                    'REMOTE_ADDR' => '123.234.123.10'
+                ],
+                ['123.234.123.10'],
+                '123.234.123.10',
+            ],
+            [
+                [
+                    'REMOTE_ADDR'          => '123.234.123.10',
+                    'HTTP_X_FORWARDED_FOR' => '222.234.123.10'
+                ],
+                ['222.234.123.10', '123.234.123.10'],
+                '222.234.123.10',
+            ],
+            [
+                [
+                    'HTTP_X_FORWARDED_FOR' => '222.234.123.10',
+                    'REMOTE_ADDR'          => '123.234.123.10'
+                ],
+                ['222.234.123.10', '123.234.123.10'],
+                '222.234.123.10',
+            ],
+            [
+                [
+                    'HTTP_X_FORWARDED_FOR' => '222.234.123.10'
+                ],
+                ['222.234.123.10'],
+                '222.234.123.10',
             ],
         ];
     }
@@ -37,7 +110,7 @@ class ClientIpTest extends Base
                 function ($request, $response, $next) {
                     $response->getBody()->write(json_encode([
                         'CLIENT_IPS' => ClientIp::getIps($request),
-                        'CLIENT_IP' => ClientIp::getIp($request),
+                        'CLIENT_IP'  => ClientIp::getIp($request),
                     ]));
 
                     return $response;
@@ -47,9 +120,80 @@ class ClientIpTest extends Base
             $headers
         );
 
-        $body = json_decode((string) $response->getBody(), true);
+        $body = json_decode((string)$response->getBody(), true);
 
         $this->assertEquals($body['CLIENT_IPS'], $CLIENT_IPS);
         $this->assertEquals($body['CLIENT_IP'], $CLIENT_IP);
+    }
+
+    /**
+     * @dataProvider srvProvider
+     */
+    public function testServerOrderCheck(array $srvOptions, array $CLIENT_IPS, $CLIENT_IP)
+    {
+        $response = $this->executeWithServer(
+            [
+                //Emulates default order
+                Middleware::ClientIp(),
+                function ($request, $response, $next) {
+                    $response->getBody()->write(json_encode([
+                        'CLIENT_IPS' => ClientIp::getIps($request),
+                        'CLIENT_IP'  => ClientIp::getIp($request),
+                    ]));
+
+                    return $response;
+                },
+            ],
+            '',
+            $srvOptions
+        );
+
+        $body = json_decode((string)$response->getBody(), true);
+
+        $this->assertEquals($body['CLIENT_IPS'], $CLIENT_IPS);
+        $this->assertEquals($body['CLIENT_IP'], $CLIENT_IP);
+    }
+
+    /**
+     * @dataProvider srvReversedProvider
+     */
+    public function testServerOrderCheckReversed(array $srvOptions, array $CLIENT_IPS, $CLIENT_IP)
+    {
+        $response = $this->executeWithServer(
+            [
+                //Emulates reversed order
+                Middleware::ClientIp()->serverOptions(['HTTP_X_FORWARDED_FOR', 'REMOTE_ADDR']),
+                function ($request, $response, $next) {
+                    $response->getBody()->write(json_encode([
+                        'CLIENT_IPS' => ClientIp::getIps($request),
+                        'CLIENT_IP'  => ClientIp::getIp($request),
+                    ]));
+
+                    return $response;
+                },
+            ],
+            '',
+            $srvOptions
+        );
+
+        $body = json_decode((string)$response->getBody(), true);
+
+        $this->assertEquals($body['CLIENT_IPS'], $CLIENT_IPS);
+        $this->assertEquals($body['CLIENT_IP'], $CLIENT_IP);
+    }
+
+    /**
+     * @param callable[] $middlewares
+     * @param string     $url
+     * @param array      $serverVars
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    protected function executeWithServer(array $middlewares, $url = '', array $serverVars = [])
+    {
+        $request = $this->request($url, [], $serverVars);
+        $response = $this->response();
+
+        return $this->dispatch($middlewares, $request, $response);
     }
 }
